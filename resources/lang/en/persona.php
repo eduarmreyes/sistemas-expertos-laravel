28bd77c7e6771db320f84bbd39e5f02cf0949f2b
eduarmreyes <eduar.mreyes@gmail.com>
2016-11-13 10:22:45 -0600

    Show labels from dictionary persona

--- resources/lang/en/persona.php
+++ resources/lang/en/persona.php
@@ -13,10 +13,28 @@ return [
     |
     */
 
-    "box_title"                             => "Persona's form",
+    // Formulario
+    "box_title"                             => "CRUD para Persona",
+    "name_label"                            => "Nombres",
+    "name_placeholder"                      => "Escribe tus nombres",
     "surname_label"                         => "Apellidos",
-    "name_label"                            => "Names",
-    "name_placeholder"                      => "Enter your names",
+    "surname_placeholder"                         => "Escribe tus apellidos",
+    "birthday_label"                         => "Fecha de nacimiento",
+    "birthday_placeholder"                         => "Tu fecha de nacimiento",
+    "correo_label"                         => "Correo",
+    "correo_placeholder"                         => "Tu correo",
+    "height_label"                         => "Estatura",
+    "height_placeholder"                         => "Tu Estatura",
+    "height_help_block"                         => "Tu estatura en metros, por ejemplo 1.74",
+    "weight_label"                         => "Peso",
+    "weight_placeholder"                         => "Tu peso",
+    "weight_help_block"                         => "Tu peso en libras, ejemplo 176",
+    "sexo_label_male"                         => "Hombre",
+    "sexo_label_female"                         => "Mujer",
+    "submit"                         => "Guardar",
+    // Table
+    "box_table_title"                         => "Tabla personas",
+    "table_caption"                         => "Esta tabla está sincronizada con la Base de Datos",
     "thead_names"                           => "Nombres",
     "thead_surnames"                        => "Apellidos",
     "thead_birthday"                        => "Cumpleaños",
@@ -25,4 +43,9 @@ return [
     "thead_weight"                          => "Peso",
     "thead_sex"                             => "Sexo",
     "thead_actions"                         => "Acciones",
+    // Notificaciones
+    "persona_from_db_notify_title"                         => "Información",
+    "persona_from_db_notify_text"                         => "Personas de la BD sincronizadas",
+    "no_persona_from_db_notify_title"                         => "Información",
+    "no_persona_from_db_notify_text"                         => "No hay personas en la BD, guarda la primera en el formulario",
 ];
